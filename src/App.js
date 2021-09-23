61a70fd6bd3b502f1ad68793f37ad29dfb782d38
Elyseeez <60116004+Elyseeez@users.noreply.github.com>
2021-09-23 16:10:24 +0300

    Fixes
    small fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import Header from './components/header';
-import Info from './components/pages/info-page';
-import About from './components/pages/about-page';
-import Culture from './components/pages/culture-page';
+import {Info, About, Culture} from './components/pages';
 import Footer from'./components/footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/app.scss';
---
+++ src/components/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Info } from './info-page';
+export { default as About } from './about-page';
+export { default as Culture } from './culture-page';
