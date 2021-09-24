5defc6bb700a44e0472b15327fbac767c41aa462
Elyseeez <60116004+Elyseeez@users.noreply.github.com>
2021-09-24 15:51:28 +0300

    links with plug

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Footer () { 
     return (
@@ -8,22 +9,22 @@ function Footer () {
                
               <div className="footer__services">
             <div className="footer__title">Services</div>
-            <li href="#">Email Marketing</li>
-            <li href="#">Campaigns</li>
-            <li href="#">Branding</li>
-            <li href="#">Offline</li>
+            <li href="#"> <Link to="#"> Email Marketing</Link></li>
+            <li href="#"><Link to="#"> Campaigns</Link></li>
+            <li href="#"><Link to="#"> Branding</Link></li>
+            <li href="#"><Link to="#"> Offline</Link></li>
           </div>
           <div className="footer__about">
             <div className="footer__title">About</div>
-            <li href="#">Our Story</li>
-            <li href="#">Benefits</li>
-            <li href="#">Team</li>
-            <li href="#">Careers</li>
+            <li href="#"><Link to="#"> Our Story</Link></li>
+            <li href="#"><Link to="#"> Benefits</Link></li>
+            <li href="#"><Link to="#"> Team</Link></li>
+            <li href="#"><Link to="#"> Careers</Link></li>
           </div>
           <div className="footer__help">
             <div className="footer__title">Help</div>
-            <li href="#">FAQs</li>
-            <li href="#">Contact Us</li>
+            <li href="#"><Link to="#"> FAQs</Link></li>
+            <li href="#"><Link to="#"> Contact Us</Link></li>
          
           </div>
           </div>
